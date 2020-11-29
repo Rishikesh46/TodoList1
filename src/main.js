63a442518b673a90756c8fa867ca9c8c10d4e4f8
Rishikesh46 <rishikesh.raut@raweng.com>
2020-11-29 20:42:30 +0530

    made taskliskpersistent

--- src/main.js
+++ src/main.js
@@ -2,6 +2,26 @@ let addToDoButton = document.getElementById('addToDo');
 let toDoContainer = document.getElementById('toDoContainer');
 let inputField = document.getElementById('inputField');
 
+function getLocalData(){
+    console.log(localStorage.getItem("toDoContainer"))
+    document.getElementById('toDoContainer').innerHTML=localStorage.getItem("toDoContainer");
+
+    let children=document.getElementById('toDoContainer').children;
+    for (var i=0;i<children.length;i++) {
+        // Do stuff
+        let item=children[i];
+        item.addEventListener('click', function(){
+            item.style.textDecoration = "line-through";
+            localStorage.setItem("toDoContainer",toDoContainer.innerHTML)
+        })
+        item.addEventListener('dblclick', function(){
+            toDoContainer.removeChild(item); 
+            localStorage.setItem("toDoContainer",toDoContainer.innerHTML)
+            //removeChild to remove a specified child node of the element
+        })
+    }
+}
+
 
 // creating eventlistener
 addToDoButton.addEventListener('click', function(){
@@ -14,9 +34,11 @@ addToDoButton.addEventListener('click', function(){
 // creating eventlistener
     para.addEventListener('click', function(){
         para.style.textDecoration = "line-through";
+        localStorage.setItem("toDoContainer",toDoContainer.innerHTML)
     })
     para.addEventListener('dblclick', function(){
         toDoContainer.removeChild(para); 
+        localStorage.setItem("toDoContainer",toDoContainer.innerHTML)
         //removeChild to remove a specified child node of the element
     })
 })
